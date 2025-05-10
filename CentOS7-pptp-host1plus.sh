9cbda829a0abd298b560a72f7cefcc63daedc183
徐久川 <82786875+jiu-chuan@users.noreply.github.com>
2025-05-10 17:04:24 +0800

    Update CentOS7-pptp-host1plus.sh

--- CentOS7-pptp-host1plus.sh
+++ CentOS7-pptp-host1plus.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-#    Setup Simple PPTP VPN server for CentOS 7 on Host1plus
-#    Copyright (C) 2015-2016 Danyl Zhang <1475811550@qq.com> and contributors
-#
-#    This program is free software; you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation; either version 2 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
 
 printhelp() {
 
